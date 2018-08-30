6faee24349ac8c412fcc66eb783d2f0214b0b6f5
Simon Bergot <simon.bergot@gmail.com>
2018-08-30 06:34:54 +0200

    use pug layouts

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var pug = require('gulp-pug');
+var del = require('del');
  
 gulp.task('html', function buildHTML() {
-  return gulp.src('pages/*.pug')
+  return gulp.src(['pages/*.pug', '!pages/_*.pug'])
   .pipe(pug({
     // Your options in here.
   }))
@@ -13,5 +14,9 @@ gulp.task('css', function buildCss() {
   return gulp.src('./*.css')
   .pipe(gulp.dest('dist'));
 });
+
+gulp.task('clean', function clean() {
+  return del('dist');
+})
 
 gulp.task('default', ['html', 'css']);
